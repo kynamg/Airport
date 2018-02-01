18c0b2b5604b1d2bc69953817c588d1e11a1d3d5
yj48 <yj48@hw.ac.uk>
2018-02-01 14:16:09 +0000

    First iteration of KioskGUI (none of the buttons work yet)

--- src/KioskGUI.java
+++ src/KioskGUI.java
@@ -25,9 +25,79 @@ public class KioskGUI implements ActionListener {
 	public KioskGUI() {
 		
 		guiFrame = new JFrame();
+		Container guiContainer = new Container();
 		
-		Container container = new Container();
-		//container.setLayout(new GridLayout());
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Check In Kiosk");
+		guiFrame.setSize(400, 150);
+		guiContainer.setLayout(new BoxLayout(guiContainer, BoxLayout.Y_AXIS));
+		
+		Font font1 = new Font("SansSerif", Font.BOLD, 20);
+		JTextField menu_location = new JTextField();
+		menu_location.setEditable(false);
+		menu_location.setText("DETAILS");
+		menu_location.setFont(font1);
+		
+		Panel details_panel = new Panel();
+		details_panel.setLayout(new GridLayout());
+		
+		details_panel.add(menu_location);
+		
+		Panel name_panel = new Panel();
+		name_panel.setLayout(new GridLayout());
+		
+		JTextField name_prompt = new JTextField();
+		name_prompt.setEditable(false);
+		name_prompt.setText("Last Name:");
+		JTextField name_entry = new JTextField();
+		name_entry.setEditable(true);
+		
+		name_panel.add(name_prompt);
+		name_panel.add(name_entry);		
+		
+		Panel booking_ref_panel = new Panel();
+		booking_ref_panel.setLayout(new GridLayout());
+		
+		JTextField booking_ref_prompt = new JTextField();
+		booking_ref_prompt.setEditable(false);
+		booking_ref_prompt.setText("Booking Reference:");
+		JTextField booking_ref_entry = new JTextField();
+		booking_ref_entry.setEditable(true);
+		
+		Panel confirm_panel = new Panel();
+		confirm_panel.setLayout(new GridLayout(1,3));
+		
+		JButton filler_1 = new JButton();
+		filler_1.setText("");
+		filler_1.setEnabled(false);
+		filler_1.setVisible(false);
+		JButton filler_2 = new JButton();
+		filler_2.setText("");
+		filler_2.setEnabled(false);
+		filler_2.setVisible(false);
+		
+		JButton enter = new JButton();
+		enter.setText("Confirm");
+		enter.setEnabled(true);
+		
+		confirm_panel.add(filler_1);
+		confirm_panel.add(filler_2);
+		confirm_panel.add(enter);
+		
+		booking_ref_panel.add(booking_ref_prompt);
+		booking_ref_panel.add(booking_ref_entry);
+		
+		guiContainer.add(details_panel);
+		guiContainer.add(name_panel);
+		guiContainer.add(booking_ref_panel);
+		guiContainer.add(confirm_panel);
+		
+		guiFrame.add(guiContainer);
+		
+		guiFrame.setVisible(true);
+		
+		/*Container container = new Container();
+		container.setLayout(new GridLayout());
 		
 		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		guiFrame.setTitle("Check In Kiosk");
@@ -36,49 +106,23 @@ public class KioskGUI implements ActionListener {
 		
 		JTextField passengerFirstName = new JTextField();
 		passengerFirstName.setEditable(true);
+		JButton passengerSubmit = new JButton();
+		passengerSubmit.setText("enter");
 		
 		JPanel passengerFirstNamePanel = new JPanel();
 		passengerFirstNamePanel.setLayout(new FlowLayout());
+		passengerFirstNamePanel.setVisible(true);
 		
 		container.add(passengerFirstName);
+		container.add(passengerSubmit);
 		container.setVisible(true);
 		
 		guiFrame.add(container);
-		guiFrame.setVisible(true);
-		
-		
-	}
-	
-	private void addNumberButton(Container parent, String name) {
-		JButton but = new JButton(name);
-		but.setActionCommand(name);
-		but.addActionListener(this);
-		parent.add(but);
-	}
-	
-	private void addActionButton(Container parent, int action, String text) {
-		JButton but = new JButton(text);
-		but.setActionCommand(text);
-		OperatorAction addAction = new OperatorAction(1);
-		but.addActionListener(addAction);
-		parent.add(but);	
+		guiFrame.setVisible(true);*/
 	}
 		
 	public void actionPerformed(ActionEvent event) {
 		String action = event.getActionCommand();
 		numberCalc.setText(action);
-	}
-	
-	private class OperatorAction implements ActionListener {
-		private int operator;
-		
-		public OperatorAction(int operation) {
-			operator = operation;			
-		}
-		
-		public void actionPerformed(ActionEvent event) {
-			currentCalc = Integer.parseInt(numberCalc.getText());
-			calcOperation = operator;
-		}	
 	}
 }
