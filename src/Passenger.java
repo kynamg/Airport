2370395dddbd6cde1bd582ee322ccf04e4e2b868
Kyna Mowat-Gosnell <kyna.mowat@gmail.com>
2018-02-01 13:49:08 +0000

    passenger variables added

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,4 +1,13 @@
-
+	import java.io.*;
 public class Passenger {
-
+	private String name;
+	private String surename;
+	private String bookingRef;
+	private String flightCode;
+	private boolean checkedIn;
+		
+	public Passenger(String name, String surname, String bookingRef, String flightCode, boolean checkedIn) {
+			
+		
+	}
 }
