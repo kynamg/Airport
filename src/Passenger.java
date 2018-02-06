0899e267f05035f17ba3f4f230212dd0ce6769a9
Kyna Mowat-Gosnell <kyna.mowat@gmail.com>
2018-02-06 10:46:15 +0000

    passenger updated

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,13 +1,48 @@
-	import java.io.*;
+/*
+Passenger Class
+*/
+
+import java.io.*;
 public class Passenger {
 	private String name;
-	private String surename;
+	private String surname;
 	private String bookingRef;
 	private String flightCode;
 	private boolean checkedIn;
 		
-	public Passenger(String name, String surname, String bookingRef, String flightCode, boolean checkedIn) {
-			
-		
+	public Passenger(String n, String s, String bR, String f, boolean cI) {
+			name = n;
+			surname = s;
+			bookingRef = bR;
+			flightCode = f;
+			checkedIn = cI;
 	}
+	
+	//return the passenger's first name
+	public String getName() {
+		return name;
+	}
+	
+	//return passenger's last name
+	public String getSurname() {
+		return surname;
+	}
+	
+	//return booking reference
+	public String bookingRef() {
+		return bookingRef;
+	}
+	
+	//return flight code
+	public String flightCode() {
+		return flightCode;
+	}
+	
+	//return whether passenger is checked in or not
+	
+	public boolean checkedIn() {
+		return checkedIn;
+	}
+	
+	
 }
