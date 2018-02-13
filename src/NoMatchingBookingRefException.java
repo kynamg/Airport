fba6ec2f8bad9126e2e55d4ccba61a22a3b184cd
Kyna Mowat-Gosnell <kyna.mowat@gmail.com>
2018-02-13 11:24:01 +0000

    NoMatchingBookingRef update

--- src/NoMatchingBookingRefException.java
+++ src/NoMatchingBookingRefException.java
@@ -1,7 +1,7 @@
-
+//Kyna Mowat-Gosnell, H00147672
 public class NoMatchingBookingRefException extends Exception {
 	public NoMatchingBookingRefException (String bookingRef) {
-		super("Invalid Booking Reference: " + bookingRef);
+		super("Booking Reference Not Found: " + bookingRef);
 	}
 
 }
